03ef24cfd680fa396261fdeb152c172dc0ebfb17
oscarliu <oscarliu@appacademy.io>
2015-09-07 15:29:39 -0400

    create toy model and collection

--- app/assets/javascripts/models/pokemon.js
+++ app/assets/javascripts/models/pokemon.js
@@ -1,8 +1,24 @@
 Pokedex.Models.Pokemon = Backbone.Model.extend({
   urlRoot: '/pokemon',
 
+  toys: function() {
+    if (this._toys === undefined) {
+      this._toys = new Pokedex.Collections.Toys();
+    };
+    return this._toys;
+  },
+
+  parse: function(payload) {
+    if (payload.toys) {
+      console.log(payload.toys);
+      this.toys().set(payload.toys);
+      delete(payload.toys);
+    }
+    return payload;
+  },
+
   toJSON: function() {
-    console.log("turned into json")
+    // console.log("turned into json")
     return {pokemon: this.attributes};
   }
 
--- app/views/pokemon/_pokemon.json.jbuilder
+++ app/views/pokemon/_pokemon.json.jbuilder
@@ -1,6 +1,6 @@
 json.extract! pokemon, :id, :attack, :defense, :image_url, :moves, :name, :poke_type
-json.toys do
-  json.array! pokemon.toys do |toy|
-    json.partial! 'toys/toy', toy: toy
+if display_toys
+  json.toys do
+    json.partial! 'toys/toy', collection: pokemon.toys, as: :toy
   end
 end
