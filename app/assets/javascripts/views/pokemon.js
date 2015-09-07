0c67562f7824a46924a7380102a25080db359270
oscarliu <oscarliu@appacademy.io>
2015-09-07 12:33:52 -0400

    can render pokemon detail in div

--- app/assets/javascripts/views/pokemon.js
+++ app/assets/javascripts/views/pokemon.js
@@ -6,13 +6,18 @@ Pokedex.Views.Pokemon = Backbone.View.extend({
     this.$toyDetail = this.$el.find('.toy-detail');
 
     this.pokemon = new Pokedex.Collections.Pokemon();
+
+    this.$pokeList.on("click", "li.poke-list-item", this.selectPokemonFromList.bind(this));
+
     this.refreshPokemon();
   },
 
   addPokemonToList: function(pokemon) {
-    var $li = $("<li>" + pokemon.get("name") + " -- " + pokemon.get("poke_type") + "</li>");
-    $li.addClass("poke-list-item");
-    this.$pokeList.append($li);
+    var $pokeli = $("<li>" + pokemon.get("name") + " -- " + pokemon.get("poke_type") + "</li>");
+    $pokeli.addClass("poke-list-item");
+    $pokeli.data({id: pokemon.get("id")});
+    this.$pokeList.append($pokeli);
+    // console.log(pokemon)
   },
 
   refreshPokemon: function() {
@@ -24,6 +29,28 @@ Pokedex.Views.Pokemon = Backbone.View.extend({
         }.bind(that))
       }
     });
+  },
+
+  renderPokemonDetail: function(pokemon) {
+    var $detail = $("<div class='detail'></div>");
+    var pokeAttrs = pokemon.attributes;
+    console.log(pokeAttrs);
+    var $img = $("<img src=" + pokeAttrs.image_url + ">");
+    $detail.append($img);
+
+    for (var attr in pokeAttrs) {
+      if (attr !== "image_url"){
+        $detail.append($("<p>" + attr + ": " + pokeAttrs[attr] + "</p>"));
+      }
+    }
+
+    this.$pokeDetail.html($detail)
+  },
+
+  selectPokemonFromList: function(event) {
+    var pokeid = $(event.currentTarget).data("id");
+    var chosenPoke = this.pokemon.get(pokeid);
+    this.renderPokemonDetail(chosenPoke);
   }
 
 });
